8d412cee3270f10e929341431b416f69afaad500
SayantanmPaul <sayantan.college6291@gmail.com>
2025-04-11 01:05:48 +0530

    fix: update deployment config (gunicorn config)

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from app.routes.spam import spam_bp 
 
-app = Flask(__name__)
-CORS(app, resources={r"/predict-spam": {"origins": "*", "allow_headers": "*", "methods": ["POST"]}})
 
-app.register_blueprint(spam_bp)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(spam_bp, url_prefix='/api')
 
-@app.route('/health', methods=['GET'])
-def health_check():
-    print("Health check ping received.")
-    return jsonify({'status': 'ok'}), 200
+    CORS(app, resources={r"/predict-spam": {"origins": "*", "allow_headers": "*", "methods": ["POST"]}})
+
+    @app.route('/health', methods=['GET'])
+    def health_check():
+        print("Health check ping received.")
+        return jsonify({'status': 'ok'}), 200
+    
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     from waitress import serve
