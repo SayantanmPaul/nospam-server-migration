36a1fb80ec8e25fc3568dfdc73da06a94d63429c
Sayantan Paul <sayantan.college6291@gmail.com>
2025-04-11 21:14:04 +0530

    fix: cors config

--- app.py
+++ app.py
@@ -5,11 +5,20 @@ from src.routes.sentiment import sentiment_bp
 
 def create_app():
     app = Flask(__name__)
+
+    # CORS config
+    CORS(app, resources={
+        r"/api/*": {
+            "origins": "*", 
+            "allow_headers": ["Content-Type", "Authorization"],
+            "methods": ["GET", "POST", "OPTIONS"]
+        }
+    })
+
+
     app.register_blueprint(spam_bp, url_prefix='/api')
     app.register_blueprint(sentiment_bp, url_prefix='/api')
 
-    CORS(app, resources={r"/predict-spam": {"origins": "*", "allow_headers": "*", "methods": ["POST"]}})
-    CORS(app, resources={r"/predict-sentiment": {"origins": "*", "allow_headers": "*", "methods": ["POST"]}})
 
     @app.route('/health', methods=['GET'])
     def health_check():
